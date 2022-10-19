6a171732ec050873b87bcd0e857c745b708e8a57
Grant Johnson <grantlj@comcast.net>
2022-10-19 11:25:50 -0700

    Initial user profile created (temporary

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Profile from './components/Profile'
 
 export default function App() {
     return ( 
         <div> 
-            <h1>Hello world</h1> 
+            <Profile />
         </div> 
 )
 }
