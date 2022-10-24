5b518ecc0c4ff648bbc2334c48382e18761ca696
ab-arias <abarias@calpoly.edu>
2022-10-24 10:50:29 -0700

    added stats frontend and style.css

--- frontend/src/components/stats.js
+++ frontend/src/components/stats.js
@@ -2,6 +2,26 @@ import React from 'react'
 
 export default function Stats() {
     return (
-        <div>stats</div>
+        <div className = 'stats-main-container'>
+            
+            <h2 className = 'section-header'> Welcome User, Lets Get This Bread </h2>
+
+            <div className = 'stats-pr-table'>
+                <div>Stats</div>
+            </div>
+
+            <div className = 'stats-diet-table'> 
+                <div>Diet</div>
+            </div>
+
+            <div className = 'stats-update-form'>
+                <div>Update Stats</div>
+            </div>
+
+            <div className = 'BMI Calculator' >
+                <div>BMI Calculator</div>
+            </div>
+        
+        </div>
     )
 }
