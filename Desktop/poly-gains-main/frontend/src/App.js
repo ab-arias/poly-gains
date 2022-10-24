0bf908bf5b7da0d38e62fdb86cb0cbb23b5e2df9
Nicholas <nmicali@calpoly.edu>
2022-10-24 00:07:26 -0700

    added cal poly link

---
+++ Desktop/poly-gains-main/frontend/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import PolyGainsLogo from './assets/img/PolyGainsLogo.png'
+import Profile from './components/Profile'
+import Stats from './components/stats'
+import CalPoly from './components/CalPoly'
+import Workouts from './components/Workouts'
+
+
+export default function App() {
+    return ( 
+        <BrowserRouter>
+            <div className='app'>
+
+                <div class="header">
+                    <div class="left-header">
+                        <Link to="/">
+                            <img style={{height: 60}} src={PolyGainsLogo} alt='PolyGains'/>
+                        </Link>
+                    </div>
+                    <div class="middle-header">
+                        <Link to="/calpoly">CalPoly</Link>
+                        <Link to="/stats">Stats</Link>
+                        <Link to="/workouts">Workouts</Link>                       
+                    </div>
+                    <div class="right-header">
+                        <p>Profile</p>
+                    </div>
+                </div>
+
+                <div className='main-body'>
+                    <Routes>
+                        <Route path='/' element={<Profile/>}/>
+                        <Route path='/stats' element={<Stats/>}/>
+                        <Route path='/calpoly' element={<CalPoly/>}/>
+                        <Route path='/workouts' element={<Workouts/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    )
+}
---
+++ Desktop/poly-gains-main/frontend/src/components/CalPoly.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function CalPoly() {
+    return (
+        <div class="CalPolyPage">
+            <div class="CalPolyHeader">
+                Cal Poly Fitness
+            </div>
+            <a  class="RecLink"
+                href="https://www.asi.calpoly.edu/facilities/recreation-center/"
+                target="_blank"
+                rel="noreferrer">
+                <div class="RecLinkButton">
+                    Cal Poly Rec Center Website!
+                </div>
+            </a>
+        </div>
+    )
+}
