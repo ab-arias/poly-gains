68f04e79f878725ae2695b9be56a5c5248d531d2
Marco Araiza <marcoaraiza@Marcos-MBP.lan>
2022-10-30 13:48:03 -0700

    linked stats to backend

--- frontend/src/components/Workouts.js
+++ frontend/src/components/Workouts.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
+
 
 export default function Workouts() {
 
@@ -13,7 +12,7 @@ export default function Workouts() {
         setCalendar({...calendar, [day]: plan})
     }
 
-    const cardContent = ['card1', 'card2', 'card3', 'card4', 'Rest']
+
 
     const cards = cardContent.map(card => 
         <div className='workouts-card' 
