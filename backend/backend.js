a0da2dcad9e284ab7b91345a2907ac18fe1e10d6
maraiza20026 <maraiza20026@gmail.com>
2022-11-09 11:09:24 -0800

    update

--- backend/backend.js
+++ backend/backend.js
@@ -9,13 +9,14 @@ const validateRegisterInput = require("./validation/register.js");
 const validateLoginInput = require("./validation/login.js");
 
 
-app.use(passport.initialize());
-require("./passport.js")(passport);
-app.use("./routes/users", users);
+app.use(cors())
+app.use(express.json())
+
 
 app.use(passport.initialize());
 require("./passport.js")(passport);
 app.use("./routes/users", users);
+
 
 app.get("/workouts", async (req, res) => {
     const name = req.query["name"];
